2f7a3260baae9d7ad9a7a51833a0662f115f6f7b
Szotkowski <mikiszotkowski@gmail.com>
2023-02-12 21:50:58 +0100

    remove lines from index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,2 @@
 export * from './login.controller';
 export * from './ping.controller';
-export * from './user-instruction.controller';
-export * from './instruction-step.controller';
-export * from './step-instruction.controller';
-export * from './instruction-user.controller';
-export * from './user-group.controller';
