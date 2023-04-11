b1aee77e5f938cd63696a9c8554bd9375e4804dd
Szotkowski <mikiszotkowski@protonmail.com>
2023-04-11 12:32:20 +0200

    proccess.env

--- src/datasources/sftp.datasource.ts
+++ src/datasources/sftp.datasource.ts
@@ -4,16 +4,17 @@ let Client = require('ssh2-sftp-client');
 let sftp = new Client();
 
 const config = {
-  host: '192.168.0.4',
-  port: '43313',
-  username: 'selecro-dev',
-  password: 'lny748*#IFisc@@',
+  host: process.env.SFTPHOST,
+  port: Number(process.env.SFTPPORT),
+  username: process.env.SFTPUSERNAME,
+  password: process.env.SFTPPASSWORD,
 };
 
 sftp.connect(config).then(() => {
-  return sftp.list('/');
+  return sftp.get("profile/1.png");
 }).then((data: any) => {
   console.log(data, 'the data info');
+  return sftp.end();
 }).catch((err: any) => {
   console.log(err, 'catch error');
 });
